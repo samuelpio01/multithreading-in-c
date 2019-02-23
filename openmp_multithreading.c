9ff7fd86645daaf30de9ddfb68a69c83acbcb012
Samuel Pio Dcosta <samuelpio01@gmail.com>
2019-02-23 17:54:59 +0530

    Add files via upload

---
+++ openmp_multithreading.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<omp.h>
+
+#define SIZE 3000
+	
+
+void unique_pair(int a[])
+{
+
+	int i, j, k, flag;
+
+	//#pragma directive with OpenMp API is used to make multiple threads to do
+	//parallel execution(multithreading) of the 'for' loop encountered next
+	
+	#pragma omp parallel for	
+	for(i=0; i<SIZE-1; i++)
+	{
+
+		for(j=i+1; j<SIZE; j++)
+		{
+			//flag is initially set to 1 before starting comparing
+			flag=1; 
+			for(k=0; k<SIZE; k++)
+			{
+				if(a[i]<a[j] && a[k]>a[i] && a[k]<a[j] && k!=i && k!=j)//if a[i]<a[j] and a[k] is in bet a[i] and a[j]
+				{
+					//flag = 0 if value any value is found between a[i] and a[j]
+					flag=0;
+					break;
+				}
+
+				else if(a[j]<a[i] && a[k]<a[i] && a[k]>a[j] && k!=i &&k!=j)//if a[i]>a[j] and a[k] is in bet a[i] and a[j]
+				{
+					//flag = 0 if value any value is found between a[i] and a[j]
+					flag=0;
+					break;
+				}
+
+			}
+			//if flag still stays 1 throughout the iterations that means no value is found in bet a[i] and a[j]
+			//and a[i] and a[j] are unique pairs
+			if(flag ==1)
+			{
+				printf("(%d,%d) or (%d,%d) = %d\n",a[i],a[j],a[j],a[i],abs(a[i]-a[j]));
+			}
+
+		}
+	}
+}
+
+
+int main()
+{
+	int a[SIZE]={0,3,5,7,10,1,2};
+	
+	int i;
+	//printing the array
+	for(i =0; i<SIZE; i++)
+		printf("%d\t",a[i]);
+	printf("\n");
+	
+	unique_pair(a);
+
+
+	return 0;
+}
+
